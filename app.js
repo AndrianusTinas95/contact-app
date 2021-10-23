fed56544c8808ab815617033bc5eee57af47adb8
andriTinas <andrianustinas.siskom@gmail.com>
2021-10-23 07:41:25 +0700

    list-contact

--- app.js
+++ app.js
@@ -3,28 +3,37 @@ const contacts = require("./contacts");
 
 
 yargs.command({
-    command:'add',
-    describe:'menambahkan kontak',
-    builder:{
-        nama:{
-            describe:'Nama Lengkap',
-            demandOption:true,
-            type:'string'
+    command: 'add',
+    describe: 'menambahkan kontak',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string'
         },
-        email:{
-            describe:'Email',
-            demandOption:false,
-            type:'string'
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string'
         },
-        noHP:{
-            describe:'No Handphone',
-            demandOption:true,
-            type:'string'
+        noHP: {
+            describe: 'No Handphone',
+            demandOption: true,
+            type: 'string'
         },
     },
-    handler(argv){
-        contacts.simpanContact(argv.nama,argv.email,argv.noHP);
-    } 
-});
+    handler(argv) {
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    }
+}).demandCommand();
+
+// menampilkan semua list contact 
+yargs.command({
+    command:'list',
+    describe:'Menampilkan semua nama & nomor hp',
+    handler(){
+        contacts.listContact();
+    }
+})
 
 yargs.parse()
--- contacts.js
+++ contacts.js
@@ -2,40 +2,51 @@ const fs = require('fs');
 const chalk = require('chalk');
 const validator = require('validator');
 
-const dirPath='./data';
-if(!fs.existsSync(dirPath)){
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
 }
-const dataPath='./data/contacts.json';
-if(!fs.existsSync(dataPath)){
-    fs.writeFileSync(dataPath,'[]','utf-8');
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const loadContacts = () => {
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+
+    return contacts;
 }
 
 
-const simpanContact = (nama,email,noHP) =>{
-    const contact       = {nama,email,noHP};
-    const fileBuffer    = fs.readFileSync('data/contacts.json','utf-8');
-    const contacts = JSON.parse(fileBuffer);
+const simpanContact = (nama, email, noHP) => {
+    const contact = {
+        nama,
+        email,
+        noHP
+    };
     
+    const contacts = loadContacts();
+
     const duplikat = contacts.find(
-        (contact) =>contact.nama === nama
+        (contact) => contact.nama === nama
     )
-    if(duplikat){
+    if (duplikat) {
         console.log(
             chalk.magenta.bold('Contact sudah terdaftar, gunakan nama lain')
         );
         return false;
     }
 
-    if(email){
-        if(!validator.isEmail(email)){
+    if (email) {
+        if (!validator.isEmail(email)) {
             console.log(
                 chalk.magenta.bold('Email Tidak valid')
             );
             return false;
         }
     }
-    if(!validator.isMobilePhone(noHP,'id-ID')){
+    if (!validator.isMobilePhone(noHP, 'id-ID')) {
         console.log(
             chalk.magenta.bold('No HP Tidak valid')
         );
@@ -44,12 +55,22 @@ const simpanContact = (nama,email,noHP) =>{
 
     contacts.push(contact);
 
-    fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
     console.log(
         chalk.green.bold('terimakasih sudah memasukan data.')
     )
-        
+
 }
 
-module.exports = {simpanContact}
+const listContact = () =>{
+    const contacts = loadContacts();
+    console.log(chalk.cyan.bold('Daftar Kontak : '));
+    contacts.forEach((contact,i)=>{
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`);
+    });
+}
+
+module.exports = {
+    simpanContact, listContact
+}
